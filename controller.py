4da2fb5f0a9845be5adbb00abdc69c0e6f911e9e
Pavlos-Efstathiou <jimosmekma@gmail.com>
2021-03-01 19:16:30 +0200

    You can now change keybinds!

--- controller.py
+++ controller.py
@@ -4,24 +4,33 @@ Packages to install: pip install pynput
 """
 import serial
 from pynput.keyboard import Key, Controller
+
 port = "COM3" # This will certainly be different on your machine ex. "COM16"
 baud = 115200
 s = serial.Serial(port) # Don't know what to name this variable
 s.baudrate = baud
- 
 keyboard = Controller()
- 
+keybinds = [str(input("Key that will be pressed when the A button is pressed:\n")), input("Key that will be pressed when the B button is pressed:\n")]
 last = 0
- 
-while 1:
+
+if keybinds[0] == "" or keybinds[1] == "":
+    print("Config file not setup will use default keybinds")
+    keybinds = ["d", Key.space]
+else:
+    for i in range(len(keybinds)):
+        if keybinds[i] == "space":
+            print()
+            keybinds[i] = Key.space
+            print("space = Key.space")
+while True:
     serial_output = int(s.read());
     if serial_output != 0:
         last = 1
     if serial_output == 1:
-        keyboard.press("d")
+        keyboard.press(keybinds[0])
     elif serial_output == 2:
-        keyboard.press(Key.space)
+        keyboard.press(keybinds[1])
     elif last != 0:
-        keyboard.release("d")
-        keyboard.release(Key.space)
+        keyboard.release(keybinds[0])
+        keyboard.release(keybinds[1])
         last = 0
